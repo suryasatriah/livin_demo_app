8371988a9636d6ad99fc24d47c0f1aacfe7f6d5f
suryahidayat <surya.hidayat@inmotion.co.id>
2024-11-18 18:05:40 +0700

    add error style

--- lib/widgets/explorer/widgets/explorer_answer_generator.dart
+++ lib/widgets/explorer/widgets/explorer_answer_generator.dart
@@ -52,7 +52,10 @@ class _ExplorerAnswerGeneratorState extends State<ExplorerAnswerGenerator>
           case ConnectionState.active:
           case ConnectionState.done:
             if (snapshot.hasError) {
-              displayText = const Text('Error');
+              displayText = Text('System currently under maintenance.',
+                  style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                        fontWeight: FontWeight.w400,
+                      ));
             } else {
               displayText = generateAnswerWidget(snapshot.data);
             }
